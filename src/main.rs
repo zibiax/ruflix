47c6915f1d8314373e73328e7318759012af3ea1
Martin <martin.evenbom@gmail.com>
2023-03-07 03:20:06 +0100

    useable video database using sqldiesel library with rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-fn main() {
-    let args = RuflixArgs::Parse();
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
 
-    match args. entity_type {
+mod args;
+mod ops;
+mod schema;
+mod db;
+mod models;
+
+use ops::user_ops::handle_user_command;
+use ops::video_ops::handle_video_command;
+use ops::view_ops::handle_view_command;
+use args::EntityType;
+use args::RuflixArgs;
+use clap::Parser;
+
+fn main() {
+    let args = RuflixArgs::parse();
+
+    match args.entity_type {
         EntityType::User(user) => handle_user_command(user),
         EntityType::Video(video) => handle_video_command(video),
         EntityType::View(view) => handle_view_command(view),
